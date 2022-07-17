55e70dca8930a478cf6bad7660aece1796aaf353
cwegner87 <cwegner1@live.maryville.edu>
2022-07-17 19:28:28 -0400

    items list

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,87 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class HomePage {
 
-  constructor() {}
+  title = "Grocery";
+
+
+  items = [
+    {
+      name: "Bread",
+      quantity: 2
+    },
+    {
+      name: "Milk",
+      quantity: 1
+    },
+    {
+      name: "Apple",
+      quantity: 3
+    },
+    {
+      name: "Soda",
+      quantity: 1
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item) {
+    console.log("Removing Item - ", item);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 
 }
